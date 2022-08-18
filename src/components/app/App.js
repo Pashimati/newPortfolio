05714fce05667fc033f11a0b5ed540202988090b
Pashimati <Pashimatii@gmail.com>
2022-08-18 21:21:19 +0300

    create base layout

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './App.scss';
+import Header from "../../containers/header/Header";
 
 function App() {
     return (
         <div className="App">
+            <Header id="pageHeader"/>
+            <article id="mainArticle">Article</article>
+            <nav id="mainNav">Nav</nav>
+            <div className="ads" id="siteAds">Ads</div>
+            <footer id="pageFooter">Footer</footer>
         </div>
     );
 }
