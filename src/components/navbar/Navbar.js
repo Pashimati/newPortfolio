6a8e347d265476c81f14895714ecf4f5995cf61a
Pashimati <Pashimatii@gmail.com>
2022-08-25 20:52:06 +0300

    add blocks info, skills, language

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,11 @@
 import "./Navbar.scss"
 import img from '../../assets/img/me.png'
-
+import inst from '../../assets/img/instagram.png'
+import github from '../../assets/img/github.png'
+import mail from '../../assets/img/email.png'
+import linkedIn from '../../assets/img/linked-big-square-logo.png'
+import telegram from '../../assets/img/telegram.png'
+import { Line } from 'rc-progress';
 const Navbar = () => {
     return (
         <nav id="mainNav">
@@ -18,54 +23,91 @@ const Navbar = () => {
                        </span>
                        <ul className="item-content__contacts nav-list-contacts">
                            <li className="nav-list-contacts__item">
-                               <img src="" alt="icon-instagram"/>
+                               <img src={inst} alt="icon-instagram"/>
                            </li>
                            <li className="nav-list-contacts__item">
-                               <img src="" alt="icon-telegram"/>
+                               <img src={telegram} alt="icon-telegram"/>
                            </li>
                            <li className="nav-list-contacts__item">
-                               <img src="" alt="icon-mail"/>
+                               <img src={mail} alt="icon-mail"/>
                            </li>
                            <li className="nav-list-contacts__item">
-                               <img src="" alt="icon-linkedIn"/>
+                               <img src={linkedIn} alt="icon-linkedIn"/>
                            </li>
                            <li className="nav-list-contacts__item">
-                               <img src="" alt="icon-github"/>
+                               <img src={github} alt="icon-github"/>
                            </li>
                        </ul>
                    </div>
                 </div>
-                <div className="grid-item">
-                    <div className="item-content">
-                        <img src="" alt=""/>
-                        <h4 className="item-content__title">
-                            Pavel Kashubovich
-                        </h4>
-                        <span className="item-content__job">
-                           Frontend developer
-                       </span>
-                        <ul className="item-content__contacts nav-list-contacts">
-                            <li className="nav-list-contacts__item">
-                                <img src="" alt="icon-instagram"/>
-                            </li>
-                            <li className="nav-list-contacts__item">
-                                <img src="" alt="icon-telegram"/>
-                            </li>
-                            <li className="nav-list-contacts__item">
-                                <img src="" alt="icon-mail"/>
-                            </li>
-                            <li className="nav-list-contacts__item">
-                                <img src="" alt="icon-linkedIn"/>
-                            </li>
-                            <li className="nav-list-contacts__item">
-                                <img src="" alt="icon-github"/>
-                            </li>
-                        </ul>
+                <div className="grid-item info-me">
+                    <div className="info-me__content">
+                        <label>Age:</label>
+                        <span>22</span>
+                    </div>
+                    <div className="info-me__content">
+                        <label>Residence:</label>
+                        <span>BY</span>
+                    </div>
+                    <div className="info-me__content">
+                        <label>Freelance:</label>
+                        <span className="active">Available</span>
+                    </div>
+                    <div className="info-me__content">
+                        <label>Address:</label>
+                        <span>Grodno, Belarus</span>
                     </div>
                 </div>
-                {/*<div className="grid-item"></div>*/}
-                {/*<div className="grid-item"></div>*/}
-                {/*<div className="grid-item"></div>*/}
+
+                <div className="grid-item abilities">
+                    <h4 className="abilities__title">Languages</h4>
+                    <div className="progress-bar">
+                        <label>English</label>
+                        <Line percent={78} strokeWidth={3} strokeColor="#FFB400" />
+                        <span>78%</span>
+                    </div>
+                    <div className="progress-bar">
+                        <label>English</label>
+                        <Line percent={33} strokeWidth={3} strokeColor="#FFB400" />
+                        <span>33%</span>
+                    </div>
+                    <div className="progress-bar">
+                        <label>English</label>
+                        <Line percent={16} strokeWidth={3} strokeColor="#FFB400" />
+                        <span>16%</span>
+                    </div>
+                </div>
+
+                <div className="grid-item abilities">
+                    <h4 className="abilities__title">Skills</h4>
+                    <div className="progress-bar">
+                        <label>English</label>
+                        <Line percent={78} strokeWidth={3} strokeColor="#FFB400" />
+                        <span>78%</span>
+                    </div>
+                    <div className="progress-bar">
+                        <label>English</label>
+                        <Line percent={33} strokeWidth={3} strokeColor="#FFB400" />
+                        <span>33%</span>
+                    </div>
+                    <div className="progress-bar">
+                        <label>English</label>
+                        <Line percent={16} strokeWidth={3} strokeColor="#FFB400" />
+                        <span>16%</span>
+                    </div>
+                </div>
+
+                <div className="grid-item abilities">
+                    <h4 className="abilities__title">Extra Skills</h4>
+                    <ul className="skills-list">
+                        <li className="skills-list__item">
+                            <span>Materialaze, Bootstrap</span>
+                        </li>
+                        <li className="skills-list__item">
+                            <span>Sass, Less, Stylus</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
