73af67060237989b94b88336e2a76e11cf191bcc
Pashimati <Pashimatii@gmail.com>
2022-08-26 13:52:22 +0300

    article add section work and education

---
+++ src/components/article/Article.js
@@ -0,0 +1,119 @@
+import './Article.scss'
+
+const Article = () => {
+    return (
+        <article id="mainArticle">
+            <div className="container-wrap">
+                <section className="section">
+                    <h2 className='section__title'>Education</h2>
+                    <p className='section__description'>
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum
+                    </p>
+                    <div className="section-box">
+                        <div className="section-box__left">
+                            <h4 className='box-title'>University Of Toronto</h4>
+                            <div className="box-label">
+                                <label>Student</label>
+                                <span>Jan 2016 - Dec 2021</span>
+                            </div>
+                        </div>
+                        <div className="section-box__right">
+                            <h4 className='box-title'>Certificate of Web Training</h4>
+                            <p className='box-text'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur dolores, excepturi iure laboriosam laudantium magni minus necessitatibus nisi placeat possimus quam quibusdam quidem reiciendis, soluta suscipit tempora, tenetur voluptate!</span></p>
+                        </div>
+                    </div>
+
+                    <div className="section-box">
+                        <div className="section-box__left">
+                            <h4 className='box-title'>University Of Toronto</h4>
+                            <div className="box-label">
+                                <label>Student</label>
+                                <span>Jan 2016 - Dec 2021</span>
+                            </div>
+                        </div>
+                        <div className="section-box__right">
+                            <h4 className='box-title'>Certificate of Web Training</h4>
+                            <p className='box-text'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur dolores, excepturi iure laboriosam laudantium magni minus necessitatibus nisi placeat possimus quam quibusdam quidem reiciendis, soluta suscipit tempora, tenetur voluptate!</span></p>
+                        </div>
+                    </div>
+
+
+                    <div className="section-box">
+                        <div className="section-box__left">
+                            <h4 className='box-title'>University Of Toronto</h4>
+                            <div className="box-label">
+                                <label>Student</label>
+                                <span>Jan 2016 - Dec 2021</span>
+                            </div>
+                        </div>
+                        <div className="section-box__right">
+                            <h4 className='box-title'>Certificate of Web Training</h4>
+                            <p className='box-text'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur dolores, excepturi iure laboriosam laudantium magni minus necessitatibus nisi placeat possimus quam quibusdam quidem reiciendis, soluta suscipit tempora, tenetur voluptate!</span></p>
+                        </div>
+                    </div>
+
+                </section>
+                <section className="section">
+                    <h2 className='section__title'>Work History</h2>
+                    <p className='section__description'>
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum
+                    </p>
+                    <div className="section-box">
+                        <div className="section-box__left">
+                            <h4 className='box-title'>University Of Toronto</h4>
+                            <div className="box-label">
+                                <label>Student</label>
+                                <span>Jan 2016 - Dec 2021</span>
+                            </div>
+                        </div>
+                        <div className="section-box__right">
+                            <h4 className='box-title'>Certificate of Web Training</h4>
+                            <p className='box-text'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur dolores, excepturi iure laboriosam laudantium magni minus necessitatibus nisi placeat possimus quam quibusdam quidem reiciendis, soluta suscipit tempora, tenetur voluptate!</span></p>
+                        </div>
+                    </div>
+
+                    <div className="section-box">
+                        <div className="section-box__left">
+                            <h4 className='box-title'>University Of Toronto</h4>
+                            <div className="box-label">
+                                <label>Student</label>
+                                <span>Jan 2016 - Dec 2021</span>
+                            </div>
+                        </div>
+                        <div className="section-box__right">
+                            <h4 className='box-title'>Certificate of Web Training</h4>
+                            <p className='box-text'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur dolores, excepturi iure laboriosam laudantium magni minus necessitatibus nisi placeat possimus quam quibusdam quidem reiciendis, soluta suscipit tempora, tenetur voluptate!</span></p>
+                        </div>
+                    </div>
+
+
+                    <div className="section-box">
+                        <div className="section-box__left">
+                            <h4 className='box-title'>University Of Toronto</h4>
+                            <div className="box-label">
+                                <label>Student</label>
+                                <span>Jan 2016 - Dec 2021</span>
+                            </div>
+                        </div>
+                        <div className="section-box__right">
+                            <h4 className='box-title'>Certificate of Web Training</h4>
+                            <p className='box-text'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur dolores, excepturi iure laboriosam laudantium magni minus necessitatibus nisi placeat possimus quam quibusdam quidem reiciendis, soluta suscipit tempora, tenetur voluptate!</span></p>
+                        </div>
+                    </div>
+
+                </section>
+                <section className="section">
+                    <h2 className='section__title'>Portfolio</h2>
+                    <p className='section__description'>
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum
+                    </p>
+                    <div className="section-box">
+
+                    </div>
+                </section>
+            </div>
+        </article>
+    );
+};
+
+export default Article;
